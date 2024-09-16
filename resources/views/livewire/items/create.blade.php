6fc251e18e140f3054eec75f7bd9acf4718ba452
zaiimrq <zaiimrq@gmail.com>
2024-09-17 08:27:51 +0900

    fix: responsive form

--- resources/views/livewire/items/create.blade.php
+++ resources/views/livewire/items/create.blade.php
@@ -26,43 +26,25 @@ new class extends Component {
 <div>
     <x-card class="shadow-md">
         <x-button label="Back" icon="o-arrow-left" link="/" class="mb-5 text-blue-500 btn-sm btn-ghost" />
-        <x-form wire:submit='save' class="grid md:grid-cols-2" id="form-item">
+        <x-form wire:submit='save' id="form-item" class="md:grid-cols-2" >
             <x-input label="Jenis Pidana" wire:model="form.jenis_pidana" />
-
             <x-input label="Nomor Register" wire:model="form.nomor_register" />
-
-            @php
-                $config = [
-                    'altFormat' => 'd M Y',
-                ];
-            @endphp
-            <x-datepicker label="Tanggal Register" icon="o-calendar" :config="$config"
+            <x-datepicker label="Tanggal Register" icon="o-calendar" :config="['altFormat' => 'd M Y']"
                 wire:model="form.tanggal_register" />
             <x-input label="Tersangka" wire:model="form.tersangka" />
-
-            <div class="col-span-2">
-                <x-textarea label="Jenis" class="" wire:model="form.jenis" />
+            <div class="md:col-span-2">
+                <x-textarea label="Jenis" wire:model="form.jenis" />
             </div>
-
             <x-input label="Golongan" wire:model="form.golongan" />
-
             <x-input label="Jumlah" type="number" wire:model="form.jumlah" />
-
             <x-input label="Satuan" wire:model="form.satuan" />
-
             <x-input label="Gudang" wire:model="form.gudang" />
-
             <x-input label="Nilai Perkiraan Awal" money prefix="Rp." local="id-ID"
                 wire:model="form.nilai_perkiraan_awal" />
-
             <x-input label="Kondisi Awal" wire:model="form.kondisi_awal" />
-
             <x-input label="Status Tingkat Pemeriksaan" wire:model="form.status_tingkat_pemeriksaan" />
-
             <x-input label="Keterangan" wire:model="form.keterangan" />
-
             <x-input label="Jaksa Penitip" wire:model="form.jaksa_penitip" />
-
             <x-input label="Tindak Pidana" wire:model="form.tindak_pidana" />
         </x-form>
         <x-slot:actions>
--- resources/views/livewire/items/show.blade.php
+++ resources/views/livewire/items/show.blade.php
@@ -19,37 +19,25 @@ new class extends Component {
         <x-button label="Back" icon="o-arrow-left" link="/" class="mb-5 text-blue-500 btn-sm btn-ghost" />
         <div class="grid gap-4 md:grid-cols-2">
             <x-input label="Jenis Pidana" value="{{ $item->jenis_pidana }}" readonly />
-
             <x-input label="Nomor Register" value="{{ $item->nomor_register }}" readonly />
             <x-input label="Tanggal Register" icon="o-calendar" value="{{ $item->tanggal_register->format('d M Y') }}"
                 readonly />
-
             <x-input label="Tersangka" value="{{ $item->tersangka }}" readonly />
-            <div class="col-span-2">
+            <div class="md:col-span-2">
                 <x-textarea label="Jenis" readonly>
                     {{ $item->jenis }}
                 </x-textarea>
             </div>
-
             <x-input label="Golongan" value="{{ $item->golongan }}" readonly />
-
             <x-input label="Jumlah" type="number" value="{{ $item->jumlah }}" readonly />
-
             <x-input label="Satuan" value="{{ $item->satuan }}" readonly />
-
             <x-input label="Gudang" value="{{ $item->gudang }}" readonly />
-
             <x-input label="Nilai Perkiraan Awal" prefix="Rp." local="id-ID"
                 value="{{ $item->nilai_perkiraan_awal }}" readonly />
-
             <x-input label="Kondisi Awal" value="{{ $item->kondisi_awal }}" readonly />
-
             <x-input label="Status Tingkat Pemeriksaan" value="{{ $item->status_tingkat_pemeriksaan }}" readonly />
-
             <x-input label="Keterangan" value="{{ $item->keterangan }}" readonly />
-
             <x-input label="Jaksa Penitip" value="{{ $item->jaksa_penitip }}" readonly />
-
             <x-input label="Tindak Pidana" value="{{ $item->tindak_pidana }}" readonly />
         </div>
 
