520bcbdb29a66fd59b02febdd53652df8669db99
zaiimrq <zaiimrq@gmail.com>
2025-02-25 21:05:09 +0900

    update .gitignore, fix asset filename, dll

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        // $this->call([ItemSeeder::class]);
+        if(app()->environment('local')) {
+            $this->call([ItemSeeder::class]);
+        }
 
         User::create([
             'name' => 'Admin',
