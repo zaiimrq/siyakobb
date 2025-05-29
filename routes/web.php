c47b398e86aa9c4ed9c2da81ee4da2a717860ec0
zaiimrq <zaiimrq@gmail.com>
2025-05-29 19:38:53 +0900

    add item download controller

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,12 @@
 <?php
 
-use App\Enums\ItemStatus;
-use App\Filters\ItemFilter;
+use App\Http\Controllers\ItemDownloadController;
 use App\Models\Item;
-use Barryvdh\DomPDF\Facade\Pdf;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('welcome');
+Route::get('/{item}', fn (Item $item) => view('items.show', ['item' => $item]))->name('items.show');
 
-Route::get(
-    '/items/{item}',
-    fn (Item $item) => view('items.show', ['item' => $item])
-)->name('items.show');
-
-Route::get('/items/all/download', function (Request $request) {
-    $filter = new ItemFilter(
-        categoryId: $request->integer('categoryId'),
-        kondisi: $request->enum('kondisi', ItemStatus::class),
-        fields: $request->array('fields')
-    );
-    $items = Item::query()
-        ->with('category')
-        ->tap($filter)
-        ->orderBy('created_at', 'desc')
-        ->get();
-    $pdf = Pdf::loadView('pdfs.all-item', ['items' => $items])->setPaper('a4', 'landscape');
-
-    return $pdf->download();
-})
+Route::get('/items/download', ItemDownloadController::class)
     ->middleware(['auth'])
     ->name('items.download');
