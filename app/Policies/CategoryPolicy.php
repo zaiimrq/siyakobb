3ee63e301ac2d99a44af6d403bb6fa4029f55a0a
zaiimrq <zaiimrq@gmail.com>
2025-06-11 18:48:46 +0900

    feat(policy): update view permissions to allow all users

--- app/Policies/CategoryPolicy.php
+++ app/Policies/CategoryPolicy.php
@@ -12,7 +12,7 @@ class CategoryPolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->isAdmin();
+        return true;
     }
 
     /**
@@ -20,7 +20,7 @@ class CategoryPolicy
      */
     public function view(User $user, Category $category): bool
     {
-        return $user->isAdmin();
+        return true;
     }
 
     /**
