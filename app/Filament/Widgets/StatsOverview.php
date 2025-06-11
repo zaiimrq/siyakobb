3c4fc8a343ec95f0cff0c1398c9fcf9cadd38ab6
zaiimrq <zaiimrq@gmail.com>
2025-06-11 20:02:58 +0900

    feat(dashboard): add clickable widget

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,24 +2,49 @@
 
 namespace App\Filament\Widgets;
 
+use App\Filament\Resources\CategoryResource;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Number;
 
 class StatsOverview extends BaseWidget
 {
+    public function redirectToDownload(): void
+    {
+        $this->redirectRoute('items.download', [
+            'fields' => Schema::getColumnListing('items'),
+        ]);
+    }
+
+    public function redirectToGolongan(): void
+    {
+        if (Auth::user()->isAdmin()) {
+            $this->redirect(CategoryResource::getUrl('index'), navigate: true);
+        }
+    }
+
     protected function getStats(): array
     {
         return [
             Stat::make('Data', Number::format($this->getTotalItems()))
                 ->description('Total data diinput')
                 ->icon('heroicon-o-document-text')
-                ->color('primary'),
+                ->color('primary')
+                ->extraAttributes([
+                    'class' => 'cursor-pointer',
+                    'wire:click' => 'redirectToDownload',
+                ]),
             Stat::make('Golongan', Number::format($this->getTotalGolongan()))
                 ->description('Total golongan')
                 ->icon('heroicon-o-tag')
-                ->color('primary'),
+                ->color('primary')
+                ->extraAttributes(Auth::user()->isAdmin() ? [
+                    'class' => 'cursor-pointer',
+                    'wire:click' => 'redirectToGolongan',
+                ] : []),
             Stat::make('Users', Number::format($this->getTotalUsers()))
                 ->description('Total users')
                 ->icon('heroicon-o-users')
