39fa8004bf5bc6839a9e11468ca8e366f620a64f
zaiimrq <zaiimrq@gmail.com>
2025-07-23 21:42:57 +0900

    fix: error on serv00

--- app/Enums/ItemStatus.php
+++ app/Enums/ItemStatus.php
@@ -7,9 +7,12 @@ use Filament\Support\Contracts\HasLabel;
 
 enum ItemStatus: string implements HasColor, HasLabel
 {
-    case BAIK = 'BAIK';
-    case RUSAK = 'RUSAK';
-    case SEBAGIAN = 'BAIK / SEBAGIAN RUSAK';
+    case BAIK = 'baik';
+    case RUSAK = 'rusak';
+    case SEBAGIAN = 'baik / sebagian rusak';
+    // case BAIK = 'BAIK';
+    // case RUSAK = 'RUSAK';
+    // case SEBAGIAN = 'BAIK / SEBAGIAN RUSAK';
 
     public function getLabel(): string
     {
