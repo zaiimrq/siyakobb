7603ef4b3601bb52b548a50dcc50fb91dc998e9a
zaiimrq <zaiimrq@gmail.com>
2025-07-26 16:26:03 +0900

    debug

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\ItemDownloadController;
 use App\Models\Item;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function () {
+    dd(now());
+})->name('debug');
+
 Route::view('/', 'welcome')->name('welcome');
 Route::get('/{item}', fn(Item $item) => view('items.show', ['item' => $item]))->name('items.show');
 
 Route::get('/items/download', ItemDownloadController::class)
     ->middleware(['auth'])
     ->name('items.download');
-
-Route::get('/debug', function () {
-    dd(now());
-})->name('debug');
