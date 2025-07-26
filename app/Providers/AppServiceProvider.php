5f75ba1b1592c40a950598fd348821728190e8b7
zaiimrq <zaiimrq@gmail.com>
2025-07-26 16:15:03 +0900

    debug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
         $this->configureModels();
         $this->configureDB();
         $this->configureDates();
+        dump(now());
 
     }
 
